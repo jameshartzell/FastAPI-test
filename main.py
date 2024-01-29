8d1ccc0f23fa170cff2051c723bc014aa4ef8636
jameshartzell <48806795+jameshartzell@users.noreply.github.com>
2024-01-29 14:16:29 -0500

    more stuff

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-#written while reading through their docs in Jan24, listening to YT vids, and three beers deep at the time of this comment
-from fastapi import FastAPI
+#written while reading through their docs on Jan24, listening to YT vids, and three beers deep at the time of this comment
+from typing import Annotated, Any, List, Union
+from fastapi import Body, Cookie, FastAPI, Header, Path
 from enum import Enum
+from pydantic import BaseModel, EmailStr, Field, HttpUrl
 
 #install dependencies w/ 'pip install "fastapi[all]"
 #start the server w/ 'uvicorn main:app --reload'
@@ -16,6 +18,66 @@ class ModelName(str, Enum):
     alexnet = "alexnet"
     resnet = "resnet"
     lenet = "lenet"
+
+#all the pyd models
+class Image(BaseModel):
+    url: HttpUrl
+    name: str
+
+#declaring a pydantic model for the expected body of calls to a POST method    
+class Item(BaseModel):
+    name: str
+    description: str | None = Field(
+        default=None, title="The description of the item", max_length=300
+    )
+    price: float = Field(gt=0, description="The price must be greater than zero")
+    tax: Union[float, None] = None
+#for just a regular array of strings
+    #tags: list[str] = []
+#for a unique array of strings
+    tags: set[str] = set()
+#another pyd model as a field
+    #image: Image | None = None
+#an array of other pyd models
+    images: list[Image] | None = None
+#providing sample data for the auto-generated docs
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "name": "Foo",
+                    "description": "A very nice Item",
+                    "price": 35.4,
+                    "tax": 3.2,
+                    "tags":['some','fuggin','tags'],
+                    "images":[]
+                }
+            ]
+        }
+    }
+
+#even more models    
+class User(BaseModel):
+    username: str
+    full_name: str | None = None
+    
+#even more nesting
+class Offer(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    items: list[Item]
+    
+class UserIn(BaseModel):
+    username: str
+    password: str
+    email: EmailStr
+    full_name: str | None = None
+    
+class UserOut(BaseModel):
+    username: str
+    email: EmailStr
+    full_name: str | None = None
 
 #minimum req for it work
 @app.get('/')
@@ -85,3 +147,184 @@ async def read_item(item_id: str, q: str | None = None):
 #bookmark https://fastapi.tiangolo.com/tutorial/query-params/
 #dropped off at heading Query parameter type conversion...
 #to be continued
+
+#everything is still an item and bools exist weow
+@app.get("/items4/{item_id}")
+async def read_item(item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+#multiple path parameters
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: str | None = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+#ITEMS
+#query params can be made to be required by leaving out default values
+@app.get("/items5/{item_id}")
+async def read_user_item(item_id: str, needy: str):
+    item = {"item_id": item_id, "needy": needy}
+    return item
+
+#I would certainly hope that whether a query params can be required or not is customizable
+#(it is)
+@app.get("/items6/{item_id}")
+async def read_user_item(
+    item_id: str, needy: str, skip: int = 0, limit: int | None = None
+):
+    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
+    return item
+
+#finally, a new section
+#and finally, POST
+@app.post("/items7/")
+async def create_item(item: Item):
+    return item
+
+#accepting both url params and a payload
+@app.put("/items8/{item_id}")
+async def update_item(item_id: int, item: Item):
+    return {"item_id": item_id, **item.dict()}
+
+#url params, a payload, and query params all in the same place
+@app.put("/items9/{item_id}")
+async def update_item(item_id: int, item: Item, q: str | None = None):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
+
+#skipping 'Query Parameters and String Validations' and 'Path Parameters and Numeric Validations' sections
+
+#
+@app.put("/items10/{item_id}")
+async def update_item(
+    item_id: Annotated[int, Path(title="The ID of the item to get", ge=0, le=1000)],
+    q: str | None = None,
+    item: Item | None = None,
+):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    if item:
+        results.update({"item": item})
+    return results
+
+#multiple body parameters (pydantic models in the payload)
+#this is how fastapi handles nested dictionaries in payloads btw
+@app.put("/items11/{item_id}")
+async def update_item(item_id: int, item: Item, user: User):
+    results = {"item_id": item_id, "item": item, "user": user}
+    return results
+
+#an individual/top level key in the payload
+@app.put("/items12/{item_id}")
+async def update_item(
+    item_id: int, item: Item, user: User, importance: Annotated[int, Body()]
+):
+    results = {"item_id": item_id, "item": item, "user": user, "importance": importance}
+    return results
+
+#skipping 'Multiple body params and query' because they've already covered it
+
+#'embed' argument to Body()
+# indents the json param in the expected payload
+@app.put("/items13/{item_id}")
+async def update_item(item_id: int, item: Annotated[Item, Body(embed=True)]):
+    results = {"item_id": item_id, "item": item}
+    return results
+
+#an endpoint with even more nesting in its expected payload
+@app.post("/offers/")
+async def create_offer(offer: Offer):
+    return offer
+
+#a body that's just an array of something
+@app.post("/images/multiple/")
+async def create_multiple_images(images: list[Image]):
+    return images
+
+#a body that's an arbitrary dict
+@app.post("/index-weights/")
+async def create_index_weights(weights: dict[int, float]):
+    return weights
+
+#skippping most of the providing example data section as it's pretty straightforward and also not necessary for in-house only tools
+
+#skipping 'Extra Data Types', but it's a good idea to take a look at it
+#----enough of the items shit holy fuck who wrote this documentation
+@app.get("/items14/")
+async def read_items(ads_id: Annotated[str | None, Cookie()] = None):
+    return {"ads_id": ads_id}
+
+#HEADERS (a little annoying that this requires an import but whatev)
+#----and even more items
+@app.get("/items15/")
+async def read_items(user_agent: Annotated[str | None, Header()] = None):
+    return {"User-Agent": user_agent}
+
+#Headers() 'convert_underscores' arg for disabling conversion from _ to - and so forth
+@app.get("/items16/")
+async def read_items(
+    strange_header: Annotated[str | None, Header(convert_underscores=False)] = None
+):
+    return {"strange_header": strange_header}
+
+#a list header
+@app.get("/items17/")
+async def read_items(x_token: Annotated[list[str] | None, Header()] = None):
+    return {"X-Token values": x_token}
+
+#setting return types to validate responses w/FastAPI
+@app.post("/items18/")
+async def create_item(item: Item) -> Item:
+    return item
+
+
+@app.get("/items19/")
+async def read_items() -> list[Item]:
+    return [
+        Item(name="Portal Gun", price=42.0),
+        Item(name="Plumbus", price=32.0),
+    ]
+    
+#setting return types to validate responses w/pyd models instead
+#the 'response_model' arg to the endpoint's decorator takes priority over specifying a return type
+@app.post("/items20/", response_model=Item)
+async def create_item(item: Item) -> Any:
+    return item
+
+
+@app.get("/items21/", response_model=list[Item])
+async def read_items() -> Any:
+    return [
+        {"name": "Portal Gun", "price": 42.0},
+        {"name": "Plumbus", "price": 32.0},
+    ]
+
+#----yes, the type defined for the body can be the same as the return type    
+# Don't do this in production!
+#----duh
+@app.post("/user/")
+async def create_user(user: UserIn) -> UserIn:
+    return user
+#----and obv don't handle PWs in plain text
+
+@app.post("/user2/", response_model=UserOut)
+async def create_user(user: UserIn) -> Any:
+    return user
